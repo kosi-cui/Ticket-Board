3ebf08efc263c2e845b67a886e213e85850f7e99
Ethan Gray <etan328@gmail.com>
2024-01-06 15:25:47 -0800

    Work on APIAgent & Unit Tests

--- src/model/api_agent.py
+++ src/model/api_agent.py
@@ -5,24 +5,114 @@ class APIAgent:
     def __init__(self, key=None, url=None):
         self.key = key
         self.url = url
+        self.current_ticket = None
+        self.filtered_ticket_list = None
+        if self.url != None and self.key != None:
+            self.valid_user = self.validKey()
+        else:
+            self.valid_user = False
+
+
+    def validKey(self) -> bool:
+        url = self.url + "/api/v2/tickets.json"
+        response = requests.get(url, auth=(self.key, "X"))
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+
+
 
     # Setters
     def setKey(self, key):
         self.key = key
+        if self.url != None:
+            self.valid_user = self.validKey()
+
+
     def setUrl(self, url):
         self.url = url
+        if self.key != None:
+            self.valid_user = self.validKey()
 
+
+    # Decorators
+    def authMethod(func):
+        def wrapper(self, *args, **kwargs):
+            if not self.valid_user:
+                return None
+            return func(self, *args, **kwargs)
+        return wrapper
+
+
+    def reqTicket(func):
+        def wrapper(self, *args, **kwargs):
+            if self.current_ticket == None:
+                return None
+            return func(self, *args, **kwargs)
+        return wrapper
+
+
+
+    # Ticket Functions -- Requires valid API Key & URL
+    @authMethod
     def ticketGetRequest(self, ticket_id):
         url = self.url + "/api/v2/tickets/" + str(ticket_id) + ".json"
         response = requests.get(url, auth=(self.key, "X"))
-        return response.json()
-    
+        if response.status_code == 200:
+            self.current_ticket = response.json()["tickets"]
+            return response.json()
+        elif response.status_code == 404:
+            return {"error": "Ticket not found."} 
+        
+
+    @authMethod
+    def filteredTicketGetRequest(self, filter):
+        if filter[0] != '"':
+            filter = f'"{filter}'
+        if filter[-1] != '"':
+            filter = f'{filter}"'
+        url = self.url + f'/api/v2/tickets/filter?query={filter}'
+        response = requests.get(url, auth=(self.key, "X"))
+        if response.status_code == 200:
+            self.filtered_ticket_list = response.json()
+            return response.json()
+        elif response.status_code == 404:
+            return {"error": "Filter not found."}
+
+    @authMethod
     def ticketPostRequest(self, ticket_id, data):
+        if self.validKey() == False:
+            return None
         url = self.url + "/api/v2/tickets/" + str(ticket_id) + ".json"
         response = requests.post(url, auth=(self.key, "X"), data=data)
         return response.json()
     
+
+    @authMethod
     def ticketPutRequest(self, ticket_id, data):
+        if self.validKey() == False:
+            return None
         url = self.url + "/api/v2/tickets/" + str(ticket_id) + ".json"
         response = requests.put(url, auth=(self.key, "X"), data=data)
         return response.json()
+
+
+
+    # Parsed Data Functions
+    @reqTicket
+    def getTicketTitle(self):
+        title = self.current_ticket["ticket"]["subject"]
+        return title
+
+
+    @reqTicket
+    def getTicketDescription(self):
+        description = self.current_ticket["ticket"]["description"]
+        return description
+
+
+    @reqTicket
+    def getTicketTags(self):
+        tags = self.current_ticket["ticket"]["tags"]
+        return tags
--- tests/agent_test.py
+++ tests/agent_test.py
@@ -5,6 +5,7 @@ from src.model.api_agent import APIAgent
 load_dotenv("../.env")
 
 
+#region Init & Setter Tests
 def test_agent_init():
     agent = APIAgent()
     assert agent.key == None
@@ -17,18 +18,67 @@ def test_agent_setters():
     assert agent.key == "123456789"
     assert agent.url == "https://test.freshservice.com"
 
-def test_env():
+def test_valid_key():
+    agent = APIAgent(url=os.getenv('HELPDESK_URL'))
+    agent.setKey("123456789")
+    assert agent.validKey() == False
+    agent.setKey(os.getenv('API_KEY'))
+    assert agent.validKey() == True
+#endregion
+    
+
+
+#region Local Environment Variable Tests -- Allows tests below to run properly
+def test_env_load():
     api_key = os.getenv('API_KEY')  # Get the value of API_KEY
     helpdesk_url = os.getenv('HELPDESK_URL')  # Get the value of HELPDESK_URL
     assert api_key != None
     assert helpdesk_url != None
+#endregion
 
-def test_agent_ticketGetRequest():
-    agent = APIAgent()
-    agent.setKey(os.getenv('API_KEY'))
-    agent.setUrl(os.getenv('HELPDESK_URL'))
+
+
+#region API Call Tests
+def test_valid_ticketGetRequest():
+    agent = APIAgent(os.getenv('API_KEY'), os.getenv('HELPDESK_URL'))
     response = agent.ticketGetRequest(os.getenv('TEST_TICKET_ID'))
     title = response["ticket"]["subject"]
     dash = title.find(" - ")
     title = title[:dash]
-    assert title == "Software Troubleshooting"
+    assert title == "Reimage"
+
+def test_unauth_ticketGetRequest():
+    agent = APIAgent()
+    agent.setKey("123456")
+    agent.setUrl(os.getenv('HELPDESK_URL'))
+    response = agent.ticketGetRequest("123456789")
+    assert response == None
+
+def test_nonexistent_ticketGetRequest():
+    agent = APIAgent(os.getenv('API_KEY'), os.getenv('HELPDESK_URL'))
+    response = agent.ticketGetRequest("123456789")
+    is_error = "error" in response
+    assert is_error == True
+
+def test_valid_filteredTicketGetRequest():
+    agent = APIAgent(os.getenv('API_KEY'), os.getenv('HELPDESK_URL'))
+    response = agent.filteredTicketGetRequest("tag:Reimage")
+    filter_exists = "error" not in response
+    assert filter_exists == True
+#endregion
+
+
+
+#region Parsing Tests
+def test_valid_getTicketTitle():
+    agent = APIAgent(os.getenv('API_KEY'), os.getenv('HELPDESK_URL'))
+    _ = agent.ticketGetRequest(os.getenv('TEST_TICKET_ID'))
+    title = agent.getTicketTitle()
+    dash = title.find(" - ")
+    assert title[:dash] == "Reimage"
+
+def test_invalid_getTicketTitle():
+    agent = APIAgent(os.getenv('API_KEY'), os.getenv('HELPDESK_URL'))
+    title = agent.getTicketTitle()
+    assert title == None
+#endregion
